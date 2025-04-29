6867c622941abfc8238bd9540115b4af51313318
Fredy-Byamasu <byamasufredy@gmail.com>
2025-04-29 12:10:49 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ console.log("hello world, " + "hello fredy");
 console.log("1234567890");
 
     // index in strings
-console.log("ASAP"[0])
-console.log("fredy"[3])
-console.log("google"[2])
+console.log("ASAP"[0]);
+console.log("fredy"[3]);
+console.log("google"[2]);
+console.log("ASAP Frontend"[12]);
+
+    //  Getting the length of a string
+console.log("ASAP".length);
+
+let string = "ASAP Frontend"
+console.log(string.length);
